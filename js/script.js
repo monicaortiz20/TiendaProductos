2353bc33446128381195b2693c27b13328cdb274
monicaortiz20 <ortiznavajomonica@gmail.com>
2022-07-06 14:44:25 +0200

    ADD/JS 3º exercise done

--- js/script.js
+++ js/script.js
@@ -2,28 +2,41 @@
 // https://fakestoreapi.com/products
 
 
+// fetch(`https://fakestoreapi.com/products`)
+//     .then(response => response.json())
+//     .then(products => console.log(products))
+
+
+// // 2 - Generar en el DOM tarjetas que contengan título, foto, descripción, precio de cada elemento.
+
+// function printProducts() {
+//     fetch(`https://fakestoreapi.com/products`)
+//         .then(response => response.json())
+//         .then(products => products.map((product) =>{
+//             let cardProduct = `<div>
+//                                     <h3>${product.title}</h3>
+//                                     <img src=${product.image}>
+//                                     <p>${product.description}</p>
+//                                     <p>${product.price}</p>
+//                                 </div>`
+
+//             document.body.innerHTML += cardProduct
+
+//         })
+//         )
+// }
+// printProducts()
+
+
+
+// 3 - Hacer un fetch a fakestoreapi para obtener las categorías de productos 
+// (Buscar en la documentación de la API el endpoint correspondiente)
+
 fetch(`https://fakestoreapi.com/products`)
     .then(response => response.json())
-    .then(products => console.log(products))
+    .then(categories => categories.map((category) => {
+        console.log(categories.category)
 
+    }))
 
-// 2 - Generar en el DOM tarjetas que contengan título, foto, descripción, precio de cada elemento.
-
-function printProducts() {
-    fetch(`https://fakestoreapi.com/products`)
-        .then(response => response.json())
-        .then(products => products.map((product) =>{
-            let cardProduct = `<div>
-                                    <h3>${product.title}</h3>
-                                    <img src=${product.image}>
-                                    <p>${product.description}</p>
-                                    <p>${product.price}</p>
-                                </div>`
-
-            document.body.innerHTML += cardProduct
-
-        })
-        )
-}
-printProducts()
-
+    //NO ME FUNCIONA
